dd0c829eb0f8757a0e12b6582c554dd045d4b0cc
pati-bivol <155124294+pati-bivol@users.noreply.github.com>
2025-11-30 13:42:53 -0800

    Initial Commit

---
+++ SkincareRoutineTracker/SkincareRoutineTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkincareRoutineTrackerApp.swift
+//  SkincareRoutineTracker
+//
+//  Created by Pati Bivol on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SkincareRoutineTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
